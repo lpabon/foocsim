2a770efd456aaa65f5642050e5a7927d69370f60
Luis Pabon <lpabon@redhat.com>
2014-06-09 11:31:15 -0400

    Cleaned up writing to the file

--- zipfdemo/zipfdemo.go
+++ zipfdemo/zipfdemo.go
@@ -13,7 +13,7 @@ func main() {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	imax := uint64(1 * 1024 * 1024 * 1024) // Max number of ints
-	s := float64(2)
+	s := float64(1.1)
 	v := float64(10)
 	z := rand.NewZipf(r, s, v, imax)
 	h := make(map[uint64]int)
@@ -21,18 +21,15 @@ func main() {
 	for i := 0; i < 20000000; i++ {
 		h[z.Uint64()] += 1
 	}
-	if false {
-		fp, err := os.Create("filecc.data")
-		godbc.Check(err == nil)
-		defer fp.Close()
-		w := bufio.NewWriter(fp)
 
-		for k, v := range h {
-			if v >= 10 {
-				_, err := w.WriteString(fmt.Sprintf("%v %d\n", k, v))
-				godbc.Check(err == nil)
-			}
-		}
+	fp, err := os.Create("filecc.data")
+	godbc.Check(err == nil)
+	defer fp.Close()
+	w := bufio.NewWriter(fp)
+
+	for k, v := range h {
+		_, err := w.WriteString(fmt.Sprintf("%v %d\n", k, v))
+		godbc.Check(err == nil)
 	}
 
 }
