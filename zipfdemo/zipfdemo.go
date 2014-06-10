5f0cc6f51dde677532e4943315b2ddeb18538219
Luis Pabon <lpabon@redhat.com>
2014-06-10 18:56:19 -0400

    Added support for choosing a file randomly

--- zipfdemo/zipfdemo.go
+++ zipfdemo/zipfdemo.go
@@ -13,36 +13,42 @@ import (
 )
 
 type SimFile struct {
-	iogen              *zipfworkload.ZipfWorkload
-	size               uint64
-	name               string
-	reads, writes, ios uint64
+	iogen *zipfworkload.ZipfWorkload
+	size  uint64
 }
 
 func main() {
+
+	var filezipf *zipfworkload.ZipfWorkload
 
 	// Setup seed for random numbers
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	// Config
 	chunksize := 256 * 1024
-	maxfilesize := int64(10*1024*1024*1024) / int64(chunksize) // Up to 10 GB in 256k chunks
-	cachesize := uint64(8*1024*1024*1024) / uint64(chunksize)  // 8 GB divided into 256k chunks
-	numfiles := 1000
+	maxfilesize := int64(1*1024*1024*1024*1024) / int64(chunksize) // Up to 1 TB in 256k chunks
+	cachesize := uint64(64*1024*1024*1024) / uint64(chunksize)     // 16 GB divided into 256k chunks
+	numfiles := 100000
 	numios := 5000000
 	deletion_chance := 15 // percent
 	read_chance := 65     // percent
 	writethrough := true
+	filedistribution_zipf := true
 
-	// Create environment
-	filezipf := zipfworkload.NewZipfWorkload(uint64(numfiles), 0)
+	// Determine distribution type
+	if filedistribution_zipf {
+		filezipf = zipfworkload.NewZipfWorkload(uint64(numfiles), 0)
+	}
+
+	// Create simulated files
 	files := make([]*SimFile, numfiles)
 	for file := 0; file < numfiles; file++ {
 		files[file] = &SimFile{}
-		files[file].size = uint64(r.Int63n(maxfilesize))
+		files[file].size = uint64(r.Int63n(maxfilesize)) + uint64(1) // in case we get 0
 		files[file].iogen = zipfworkload.NewZipfWorkload(files[file].size, read_chance)
-		files[file].name = strconv.Itoa(file)
 	}
+
+	// Print here Simulation information, also Mean file size and std deviation
 
 	// Setup file to write cache metrics
 	fp, err := os.Create("cache.data")
@@ -66,7 +72,13 @@ func main() {
 		}
 
 		// Get the file
-		file, _ := filezipf.ZipfGenerate()
+		var file uint64
+		if filedistribution_zipf {
+			file, _ = filezipf.ZipfGenerate()
+		} else {
+			// Random Distribution
+			file = uint64(r.Int63n(int64(numfiles)))
+		}
 		godbc.Check(int(file) <= numfiles, fmt.Sprintf("file = %v", file))
 
 		// Check if we need to delete this file
@@ -77,15 +89,9 @@ func main() {
 
 		// Which block on the file
 		chunk, isread := files[file].iogen.ZipfGenerate()
-
-		// Track the number of IOs to this file
-		files[file].ios++
-
 		if isread {
-			files[file].reads++
 			cache.Read(strconv.FormatUint(file, 10), strconv.FormatUint(chunk, 10))
 		} else {
-			files[file].writes++
 			cache.Write(strconv.FormatUint(file, 10), strconv.FormatUint(chunk, 10))
 		}
 	}
