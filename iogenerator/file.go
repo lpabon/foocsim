134192d1546296e08bb1d357c26bccf824b19a5e
Luis Pabón <lpabon@redhat.com>
2015-01-05 15:24:51 -0500

    SPC1 support
    Instead of using the ZipF distribution, foocsim now uses an
    SPC1-like OLTP distribution developed by NetApp.
    
    Signed-off-by: Luis Pabón <lpabon@redhat.com>

--- iogenerator/file.go
+++ iogenerator/file.go
@@ -16,22 +16,38 @@
 package iogenerator
 
 import (
-	"github.com/lpabon/foocsim/zipfworkload"
+	"github.com/lpabon/goioworkload/spc1"
 )
 
 type File struct {
-	iogen *zipfworkload.ZipfWorkload
+	iogen *spc1.Spc1Io
 	size  uint64
 }
 
 func NewFile(size uint64, readp int) *File {
 	f := &File{}
-	f.iogen = zipfworkload.NewZipfWorkload(size, readp)
+	spc1.Spc1Init(
+		100, //bsus: Doesn't matter since we do not use timing
+		1,   //contexts
+		uint32(size/(2*4096)),           // asu1 in 4k blocks
+		uint32(size/(2*4096)),           // asu2 in 4k blocks
+		uint32((float64(size)/0.9)*0.1), // asu3, unsused
+	)
+	f.iogen = spc1.NewSpc1Io(1)
 	f.size = size
 
 	return f
 }
 
 func (f *File) Gen() (uint64, bool) {
-	return f.iogen.ZipfGenerate()
+	if f.iogen.Blocks <= 0 {
+		f.iogen.Generate()
+		for f.iogen.Asu == 3 {
+			f.iogen.Generate()
+		}
+	}
+	offset := uint64((uint32(f.size/2)*(f.iogen.Asu-1))+f.iogen.Offset) * 4096
+	f.iogen.Offset++
+	f.iogen.Blocks--
+	return offset, f.iogen.Isread
 }
